850c6d07a0684fb86261b239a6bb31de1955ce06
沫雨 <1291953408@qq.com>
2020-03-13 11:06:54 +0800

    add cache

---
+++ byteview.go
@@ -0,0 +1,33 @@
+// lru
+// @Time : 2020/3/13 9:59
+// @Author : Jeffery Sun
+// @File : byteview
+// @Software: GoLand
+
+package geecache
+
+// AByteView holds an immutable view of bytes
+type ByteView struct {
+	b []byte
+}
+
+// Len returns the view's length
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// ByteSlice return s a copy of the data as a byte slice.
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// String returns the data as a string, making a copy if necessary.
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
