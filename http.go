6598f4a87eb634b009662b8bb5fe745009b9bdad
沫雨 <1291953408@qq.com>
2020-03-16 10:17:20 +0800

    add http server

--- http.go
+++ http.go
@@ -5,3 +5,65 @@
 // @Software: GoLand
 
 package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+//
+type HTTPPool struct {
+	// this perr's, base URL, e.g. "https://example.net:8000"
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+// Log info with server name
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+// ServeHTTP handle all http requests
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	fmt.Println(r.URL.Path[len(p.basePath):])
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	fmt.Println(parts)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group:"+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
